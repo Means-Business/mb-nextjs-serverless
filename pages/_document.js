e76db8990747d291bdc22e7acc88f07defe8dff5
ManotLuijiu <manot_luijiu@hotmail.com>
2019-01-12 18:57:31 +0700

    npm i again

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,16 @@
+/* eslint-disable */
 import Document, { Head, Main, NextScript } from 'next/document';
-
 
 export default class DocumentWrapper extends Document {
   render() {
     return (
       <html>
         <Head>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+            key="viewport"
+          />
           <title>NextJS | Serverless | Express</title>
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link rel="icon" type="image/png" href="/static/images/favicon.png" />
